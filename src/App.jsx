1c0b02e24bb7b0985b69c2704d5ba77f5af5285e
willi <williamnabulele1@gmail.com>
2026-03-12 12:23:27 +0000

    chore: removed default Vite boilerplate and set up Kodo landing page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css';
+
+function App() {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '10vh', fontFamily: 'sans-serif' }}>
+      <h1>Kodo</h1>
+      <p>Personal Safety Monitor</p>
+    </div>
+  );
+}
+
+export default App;
